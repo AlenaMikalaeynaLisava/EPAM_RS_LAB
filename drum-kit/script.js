697278d9def0e67c22fc01325b41ae55cbe67bdd
AlenaMikalaeynaLisava <alenalisava87@gmail.com>
2020-11-29 14:21:42 +0300

    fix: change keyCode to .code

--- drum-kit/script.js
+++ drum-kit/script.js
@@ -10,8 +10,8 @@ el.addEventListener('transitionend', removeTransition);
 })
 
 const presKey = (e) => {
- const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+ const audio = document.querySelector(`audio[data-key="${e.code}"]`);
+    const key = document.querySelector(`.key[data-key="${e.code}"]`)
     if(!audio) return;
     audio.currentTime =0;
     audio.play();
